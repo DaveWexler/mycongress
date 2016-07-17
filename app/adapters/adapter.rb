56be87a9377c4201fb06a1af263a480a636c9bd4
Dan Gorman <danielhgorman@gmail.com>
2016-07-17 15:20:09 -0400

    added adapter

---
+++ app/adapters/adapter.rb
@@ -0,0 +1,29 @@
+
+require 'httparty'
+require 'pry'
+module Adapter
+class CongressGem
+
+  include HTTParty
+
+  BASE_URL = 'https://congress.api.sunlightfoundation.com/legislators'
+  API_KEY = '15f7888634d244858926287bbd6222d9'
+
+  def search(page)
+    response = self.class.get(BASE_URL, { query: {page: page, per_page: 50, apikey: API_KEY}})
+  end
+
+  def run
+      page_num = 0 
+      politicians = []
+      while page_num < 12
+          politicians << self.search(page_num)["results"]
+          page_num += 1
+      end
+      politicians.flatten!.uniq!
+  end
+
+
+end
+end
+Pry.start 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+include Adapter
+	client = Adapter::CongressGem.new
+	@politicians = client.run
+end
