7802e5a0a09a6ef8420dcdbde4db744efa0243dc
DaveWexler <dwexler@gmail.com>
2016-07-18 19:12:20 -0400

    add styling to politician show page, add heroic features customization

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,7 +12,7 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:user][:email])
     if user == nil
-      flash[:message] = 'A user with that email address does not exist. Please create an account.'
+      flash.now[:message] = 'A user with that email address does not exist. Please create an account.'
       render 'new'
     elsif user.authenticate(params[:user][:password])
       session[:user_id] = user.id
