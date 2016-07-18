091750464ef3b85b71a8eef71252fa67dbce737d
greatermeans <gakouri@gmail.com>
2016-07-18 10:19:02 -0400

    filter method working

--- app/controllers/politicians_controller.rb
+++ app/controllers/politicians_controller.rb
@@ -5,36 +5,42 @@ class PoliticiansController < ApplicationController
   end
 
   def index
-  	if params['query'].nil?
+    if params['query'].nil?
       @politicians = Politician.order('Random()').first(9)
     else
       @politicians = search.first(9)
-  	end
+    end
   end
 
   private
 
   def query_params
-    params.require(:query).permit(:search_value, :filter_by, :search_by)
+    params.require(:query).permit(:search_value, :search_by, search_filter: [])
   end
 
   def search_method
-  	Politician::SEARCH_BY["#{query_params.values[2]}"]
+    Politician::SEARCH_BY["#{query_params.values[1]}"]
   end
 
   def search_value
-  	query_params.values[0].capitalize
+    query_params.values[0].capitalize
   end
 
   def filter_method
-  	query_params.values[1].split.map(&:downcase).join('_')
+    query_params.values[2].map(&:downcase).map {|value| value.split.join('_')}
   end
 
   def search
     if search_method.nil?
-      Politician.send(filter_method)
+      send_chain
     else
-      Politician.send(search_method,search_value).send(filter_method)
+      Politician.send(search_method,search_value).send_chain
     end
   end
+
+  def send_chain
+    filter_method.inject(Politician) {|o,a| o.send(a)}
+  end
+
+
 end
