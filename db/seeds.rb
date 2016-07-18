4a1f87852ab4bf2c0130ce3113c0a66a83b23dab
greatermeans <gakouri@gmail.com>
2016-07-18 11:06:58 -0400

    seed file to adapter

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,20 +7,25 @@
 #   Character.create(nam] = 'Luke' movi] = movies.first)
 
 
-client = Adapter::Legislators.new
-@politicians = client.run
+
 
 User.destroy_all
 Politician.destroy_all
 State.destroy_all
 District.destroy_all
-run
+
+client = Adapter::Legislators.new
+client.run
+
 District.where(name: nil).destroy_all
+
+
 jon = User.new(first_name: 'Jon', last_name: 'Log', password: 'one', email: 'jonlog@gmail.com', street_address: '11 Broadway', city: 'New York', state: State.find_by(name: 'New York'))
 jeremy = User.new(first_name: 'Jeremy', last_name: 'Won', password: 'one', email: 'jeremywon@aol.com', street_address: '2611 N Central Ave', city: 'Phoenix', state: State.find_by(name: 'Arizona'))
 lea = User.new(first_name: 'Lea', last_name: 'Bent', password: 'one', email: 'leabent@gmail.com', street_address: '15 Twilight Dr', city: 'Foxboro', state: State.find_by(name: 'Massachusetts'))
 irene = User.new(first_name: 'Irene', last_name: 'Left', password: 'one', email: 'ireneleft@gmail.com', street_address: '132 N Main St', city: 'Concord', state: State.find_by(name: 'New Hampshire'))
 willy = User.new(first_name: 'Jon', last_name: 'Wonka', password: 'one', email: 'willywonka@gmail.com', street_address: '2801 Main St', city: 'Irvine', state: State.find_by(name: 'California'))
+
 Adapter::Districts.new(jon)
 Adapter::Districts.new(jeremy)
 Adapter::Districts.new(lea)
